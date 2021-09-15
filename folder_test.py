4e6ed0c8421f1f19da047048bdd5a4378f2d9207
pwildsmith <pwildsmith@ebay.com>
2021-09-15 09:58:06 +0200

    fire on pull_request_target (#2273)
    * fire on pull_request_target
    
    * check out the base and merge commits separately, and use the base commit to test the merge commit
    
    * check out target after testing tests
    
    Co-authored-by: gdelaney <gdelaney@ebay.com>

--- folder_test.py
+++ folder_test.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3.6
 
+import sys
+
 from foldercheck import foldertest
 
 
 if __name__ == "__main__":  # pragma: no cover
-    test = foldertest.squidcheck()
+    test = foldertest.squidcheck(sys.argv[1] or ".")
     test.check()
